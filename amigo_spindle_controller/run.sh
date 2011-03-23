e81d534cee558263630ec0a1ef2daf8ec809657d
Niek Bilterijst <n.e.bilterijst@student.tue.nl>
2011-03-23 12:21:42 +0000

    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@2685 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- amigo_spindle_controller/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rosrun xacro xacro.py `rospack find amigo_spindle_controller`/deployement_configuration.xml.xacro -o `rospack find amigo_spindle_controller`/deployement_configuration.xml
-
--- amigo_spindle_controller/run_su.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export ROS_MASTER_URI=http://localhost:11311
-#export ORO_LOGLEVEL=4
-
-#roscturtle
-
-/home/amigo2/ros/orocos_toolchain_ros/ocl/bin/deployer-gnulinux -s /home/amigo2/ros/svn/dev/Niek/amigo_spindle_controller/deployement_configuration.xml
-
