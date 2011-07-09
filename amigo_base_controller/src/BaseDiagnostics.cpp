ce535951ba0d8773af0ca2ea22be7ab6d83642a7
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2011-07-09 16:08:36 +0000

    Adding diagnostics to base
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@5182 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ amigo_base_controller/src/BaseDiagnostics.cpp
@@ -0,0 +1,100 @@
+/** BaseDiagnostics.cpp
+ *
+ * @class BaseDiagnostics
+ *
+ * \author Tim Clephas
+ * \date March, 2011
+ * \version 1.0
+ *
+ */
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <ocl/Component.hpp>
+
+#include "BaseDiagnostics.hpp"
+
+using namespace RTT;
+using namespace AMIGO;
+
+BaseDiagnostics::BaseDiagnostics(const string& name) :
+	                    		    TaskContext(name, PreOperational)
+{
+
+  // Adding ports
+  addEventPort( "safe", safeport );
+  //addEventPort( "rosdiagnostics", rosdiagnosticsport );
+  //addEventPort( "rosemergency", rosemergencyport );
+  addPort( "diagnostics", diagnosticsport );
+  addEventPort( "pos", posport );
+  addPort( "integratordiagnostics", integratordiagnosticsport );
+}
+
+BaseDiagnostics::~BaseDiagnostics(){}
+
+bool BaseDiagnostics::configureHook()
+{
+
+  return true;
+}
+
+bool BaseDiagnostics::startHook()
+{
+
+
+  return true;
+}
+
+void BaseDiagnostics::updateHook()
+{
+	vector<diagnostic_msgs::DiagnosticStatus> statuses;
+
+	diagnostic_updater::DiagnosticStatusWrapper status;
+
+
+	    static bool first = true;
+	        if (first)
+	        {
+	          first = false;
+	          status.add("Robot Description", "AMIGO");
+	        }
+	        status.addf("Max EtherCAT roundtrip (us)", "%.2f", 5);
+	        status.addf("Avg EtherCAT roundtrip (us)", "%.2f", 7);
+
+		status.name = "Realtime Control Loop";
+		    if (false) // Warning
+		    {
+		      if (true)
+			status.level = 1;
+		      else
+			status.level = 0;
+		      status.message = "Realtime loop used too much time in the last 30 seconds.";
+		    }
+		    else
+		     {
+		       status.level = 0;
+		       status.message = "OK";
+		     }
+
+		    if (false)
+		        {
+		          status.mergeSummaryf(status.ERROR, "Halting, realtime loop only ran at %.4f Hz", 77);
+		        }
+							;
+
+
+
+
+		    statuses.push_back(status);
+
+
+	diagnostic_msgs::DiagnosticArray msg;
+
+	msg.status=statuses;
+
+  		  diagnosticsport.write( msg );
+  		  log(Info)<<"publish"<<endlog();
+
+}
+
+ORO_CREATE_COMPONENT(AMIGO::BaseDiagnostics)
