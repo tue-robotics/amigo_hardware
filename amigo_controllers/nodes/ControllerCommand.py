af8995b9e3a2481cda76568aff511b0765c78623
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2011-09-27 09:29:07 +0000

    Better
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@5753 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- amigo_controllers/nodes/ControllerCommand.py
+++ amigo_controllers/nodes/ControllerCommand.py
@@ -3,17 +3,22 @@ import roslib; roslib.load_manifest('amigo_controllers')
 
 from amigo_controllers.srv import *
 import rospy
-from subprocess import call
-from os import popen
+#from subprocess import call, Popen
+#import subprocess as sp
+from os import system
 
 def enable_controller(req):
-	print "Returning [%s + %s]"%(req.controller_number, req.command)
 	
 	#RUN LOCALLY ON AMIGO2
 	#preamble="ssh amigo@amigo2 'source /opt/ros/diamondback/setup.bash; ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/ros; ROS_MASTER_URI=http://amigo1:11311; "
 	controllers_exist = [ False, False, False, False, False ]
 
-	package_names = ("amigo_base_controller", "amigo_spindle_controller", "arm_left", "arm_right", "dynamixel_ethercat")
+	package_names = ("amigo_base_controller", "amigo_spindle_controller", "arm_left", "arm_right", "amigo_head_controller")
+	commands_prefixes = ("base_controller", "spindle_controller", "pera", "pera", "head_controller")
+	allowed_commands = ("enable", "reset", "disable")
+	if not req.command in allowed_commands:
+		print "Command not valid!"
+		return 0
 		
 	
 	for (counter, package_name) in enumerate(package_names):	
@@ -22,27 +27,33 @@ def enable_controller(req):
 			controllers_exist[counter] = True
 	
 	print controllers_exist
-		
+
 		
 	if ( req.command == "enable" ):
 		if ( controllers_exist[req.controller_number] ):
 			print "start ", req.controller_number
-			#call("rosrun ocl cdeployer-gnulinux -s `rospack find amigo_base_controller`/base_controller_start.ops & sleep 5; kill $!")
+			cmd = "rosrun ocl cdeployer-gnulinux -s `rospack find " + package_names[req.controller_number] + "`/" + command_prefixes[req.controller_number] + "_start.ops & sleep 5; kill $!"
+			system(cmd)
 		else:
 			print "kill all, load all, start ", req.controller_number
-			call("/etc/ros/setup.bash; roslaunch amigo_launch_files all_etherCAT_hardware.launch")
+			system("ps x | grep deployer | grep -v grep | awk '{print $1}' | xargs kill")
+			system("roslaunch amigo_launch_files all_etherCAT_hardware.launch")
 
 		
-	if ( req.command == "disable" ):#and controllers_exist[req.controller_number] ):
-		stop_commands = ("base_controller_stop.ops", "spindle_controller_stop.ops", "arm_left_stop.ops", "arm_right_stop.ops", "dynamixel_ethercat_stop.ops")
-		command = "rosrun ocl cdeployer-gnulinux -s `rospack find " + package_names[req.controller_number] + "`/" + stop_commands[req.controller_number] + " & sleep 5; kill $!"
-		#command = "roscore"
-		print command
-		call(command)
+	if ( req.command == "disable" and controllers_exist[req.controller_number] ):
+		cmd = "rosrun ocl cdeployer-gnulinux -s `rospack find " + package_names[req.controller_number] + "`/" + command_prefixes[req.controller_number] + "_stop.ops & sleep 5; kill $!"
+		print cmd
+		system(cmd)
+
+		
+	if ( req.command == "reset" and controllers_exist[req.controller_number] ):
+			system("ps x | grep deployer | grep -v grep | awk '{print $1}' | xargs kill")
+			system("roslaunch amigo_launch_files all_etherCAT_hardware.launch")
+
+
+		#process = sp.Popen(cmd.split(" "), stdout=sp.PIPE, stderr=sp.PIPE, stdin=sp.PIPE, bufsize=1)
 
 #ps x | grep amigo_base_controller | grep -v grep | awk '{print $1}' | xargs kill
-
-	
 	
 	return ControllerCommandResponse(1)
 
