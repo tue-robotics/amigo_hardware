1e4964cbcc6af44f7ad95ec319a729790836b75b
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2011-09-27 12:53:32 +0000

    Added head_controller for naming consistency
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@5762 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ amigo_head_controller/src/PanTiltController.hpp
@@ -0,0 +1,102 @@
+/***************************************************************************
+ tag: Sava Marinkov  Thu May 12 12:50:00 CET 2011  PanTiltController.hpp
+
+ PanTiltController.hpp -  description
+ -------------------
+ begin                : Sat February 19 2011
+ copyright            : (C) 2011 Sava Marinkov
+ email                : s.marinkov@student.tue.nl
+
+ ***************************************************************************/
+
+#ifndef PANTILTCONTROLLER_HPP
+#define PANTILTCONTROLLER_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <ocl/Component.hpp>
+#include <soem_beckhoff_drivers/CommMsgBig.h>
+#include <amigo_msgs/head_ref.h>
+#include <std_msgs/Float64.h>
+#include "AX12_control_table.h"
+#include <queue>
+
+#define RAD_TO_STEP             195.37860814
+#define TRIAL_MAX 				20
+
+using namespace RTT;
+using namespace soem_beckhoff_drivers;
+
+namespace AMIGO {
+
+typedef std::vector<double> doubles;
+
+class PanTiltController : public RTT::TaskContext {
+	private:
+
+		OutputPort<CommMsgBig> instructionPort;
+		InputPort<CommMsgBig> statusPort;
+		InputPort<amigo_msgs::head_ref> goalPosPort;
+		OutputPort<std_msgs::Float64> currentPosPanPort;
+		OutputPort<std_msgs::Float64> currentPosTiltPort;
+		
+		InputPort<bool> serialRunningPort;
+		InputPort<bool> serialReadyRxPort;
+		
+		CommMsgBig instruction;
+		CommMsgBig status;
+		amigo_msgs::head_ref goalPos;
+		int trial, newPosition;
+		std_msgs::Float64 currentPan, currentTilt;
+		
+		int state, pstate;
+		int commStatus;
+		unsigned char gbInstructionPacket[MAXNUM_TXPARAM+10];
+		unsigned char gbStatusPacket[MAXNUM_TXPARAM+10];
+		int gbStatusSize;
+		int pan_id, tilt_id, pan_goal, tilt_goal, pan_max, pan_min, tilt_max, tilt_min, pan_speed, tilt_speed, pan_offset, tilt_offset;
+	public:
+		PanTiltController(const std::string& name);
+		~PanTiltController(){};
+
+		bool configureHook();
+
+		bool startHook();
+	  
+		void updateHook();
+
+		void stopHook(){};
+	
+	private:
+		bool readReference();
+	
+		void dxl_set_txpacket_instruction(int instruction);
+		void dxl_tx_rx_packet(void);
+		void dxl_set_txpacket_parameter(int index, int value);
+		void dxl_set_txpacket_length(int length);
+		
+		void dxl_rx_packet(void);
+		int dxl_get_rxpacket_error(int errbit);
+		int dxl_rxpacket_isError(void);
+		int dxl_get_rxpacket_length(void);
+		int dxl_get_rxpacket_id(void);
+		int dxl_get_rxpacket_parameter(int index);
+		bool dxl_check_rxpacket_checksum(void);
+		
+		int dxl_makeword(int lowbyte, int highbyte);
+		int dxl_get_lowbyte(int word);
+		int dxl_get_highbyte(int word);
+
+		void dxl_ping(int id);
+		void dxl_read_byte(int id, int address);
+		void dxl_write_byte(int id, int address, int value);
+		void dxl_read_word(int id, int address);
+		void dxl_write_word(int id, int address, int value);
+		
+		void dxl_tx_packet(void);
+		void dxl_set_txpacket_id( int id );
+		void printErrorCode(void);
+
+};
+}
+#endif
