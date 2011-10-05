a77b5cb96adbd8a6a55933ec910fe09558eeeb5b
Bas Willems <b.willems@student.tue.nl>
2011-10-05 10:00:27 +0000

    Pushing dev to trunk. Now includes active breaking.
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@5855 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ amigo_pera_controller/src/PERA_GripperControl.cpp
@@ -0,0 +1,102 @@
+/**************************************************************************
+ *                                                                        *
+ *   S. Marinkov                                                           *
+ *   Eindhoven University of Technology                                   *
+ *   2011                                                                 *
+ *                                                                        *
+ **************************************************************************/
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <ocl/Component.hpp>
+
+#include "PERA_GripperControl.hpp"
+
+#define gripperGain 0.040
+#define MAX_TORQUE 100.0
+
+using namespace RTT;
+using namespace PERA;
+
+	GripperControl::GripperControl(const std::string& name)
+        : TaskContext(name, PreOperational)
+
+		{
+			// Creating the ports
+			
+			/// Inports
+			addPort("gripper_close", gripperClosePort);
+			addPort("torque_in", torqueInPort);
+			addEventPort("resetGripperPort",resetGripperPort);
+			
+			/// Outports
+			addPort("gripper_ref",gripperRefPort);
+			addPort("gripper_status",gripperStatusPort);
+			
+			/// Thresholds for the gripper force
+			addProperty( "threshold_closed", threshold_closed);			
+					
+	  }
+
+	GripperControl::~GripperControl(){}
+
+	bool GripperControl::configureHook(){
+		torques.resize(8);
+		gripperPos.resize(1.0,0.0);
+		completed = true;
+		return true;
+	}
+
+	bool GripperControl::startHook(){
+		return true;
+	}
+
+	void GripperControl::updateHook(){
+		
+		bool resetGripper;
+		
+		if (resetGripperPort.read(resetGripper) == NewData){
+			if(resetGripper){
+				gripperPos[0]=0.0;
+			}
+		}
+		
+		if (gripperClosePort.read(gripperClose) == NewData){
+			completed = false;
+		}
+		
+		if (!completed){
+			torqueInPort.read(torques);
+			if(!gripperClose.data){
+				if (gripperPos[0] >= 3.5){
+					log(Info)<<"Gripper is OPEN"<<endlog();
+					std_msgs::Bool gripperStatus;
+					gripperStatus.data = false;
+					gripperStatusPort.write(gripperStatus);
+					completed = true;
+				} 
+				else{
+					gripperPos[0] += gripperGain*PI/180;
+				}
+			} 
+			else{
+				//log(Warning)<<"gripper torques = "<<torques[GRIPPER_JOINT_TORQUE_INDEX]<<endlog();
+				if (torques[GRIPPER_JOINT_TORQUE_INDEX] >= threshold_closed && torques[GRIPPER_JOINT_TORQUE_INDEX] < MAX_TORQUE){
+					log(Warning)<<"Gripper is CLOSED"<<endlog();
+					std_msgs::Bool gripperStatus;
+					gripperStatus.data = true;
+					gripperStatusPort.write(gripperStatus);
+					completed = true;
+				} 
+				else if(torques[GRIPPER_JOINT_TORQUE_INDEX] < threshold_closed && torques[GRIPPER_JOINT_TORQUE_INDEX] < MAX_TORQUE){
+					//log(Warning)<<"GRIPPERCON: closing with torque = "<<torques[GRIPPER_JOINT_TORQUE_INDEX]<<endlog();
+					gripperPos[0] -= gripperGain*PI/180;
+				}
+			}
+			gripperRefPort.write(gripperPos);
+		}
+	}
+
+void GripperControl::stopHook(){}
+
+ORO_CREATE_COMPONENT(PERA::GripperControl)
+
---
+++ amigo_pera_controller/src/PERA_VectorConcatenate.hpp
@@ -0,0 +1,67 @@
+/**************************************************************************
+ *                                                                        *
+ *   B. Willems                                                           *
+ *   Eindhoven University of Technology                                   *
+ *   2011                                                                 *
+ *                                                                        *
+ **************************************************************************/
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <std_msgs/Bool.h>
+
+using namespace RTT;
+
+namespace PERA
+{
+	typedef std::vector<double> doubles;
+	
+	/*! \class Concatenate
+	 *  \brief Defines Orocos component for vector concatenation (PERA
+	 *   only, non generic)
+	 * 
+	 * This component was created because the standard vector 
+	 * concatenate does not use eventsports thus running at a fixed
+	 * frequency. Component only developed for PERA application
+	 * therefor not generic.
+	 */
+	
+	class Concatenate : 
+
+	  public RTT::TaskContext
+	  {
+	  private:
+	  
+	  //! Inputport for first vector
+	  InputPort<doubles> invector1Port;
+	  //! Inputport for second vector
+	  InputPort<doubles> invector2Port;
+	  //! Outputport for outputting the concatenated vector
+	  OutputPort<doubles> outPort;
+	  
+	  //! First vector
+	  doubles invector1;
+	  //! Second vector
+	  doubles invector2;
+	  //! Outputvector
+	  doubles outvector;
+	  
+	public:
+
+	  Concatenate(const std::string& name);
+	  
+	  ~Concatenate();
+	  
+	  //! Configuration sequence, executed before startHook()
+	  bool configureHook();
+	  //! Starting sequence, executed once upon startup of the component
+	  bool startHook();
+	  //! Update sequence, performed at specified rate
+	  void updateHook();
+	  //! Stopping sequence, executed once upon stop of the component
+	  void stopHook();
+	  	  
+	};
+	
+};
+
