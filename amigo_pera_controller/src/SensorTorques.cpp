1406aa54443d9f67f924ca0b9fcc8263fd8c7774
Tim Clephas <t.t.g.clephas@alumnus.tue.nl>
2011-11-08 21:19:15 +0000

    Added SensorTorques to amigo_pera_controller and changed .ops file to work with rtt_generic components
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@5943 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ amigo_pera_controller/src/SensorTorques.cpp
@@ -0,0 +1,73 @@
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <ocl/Component.hpp>
+
+#include "SensorTorques.hpp"
+
+using namespace RTT;
+using namespace AMIGO;
+
+SensorTorques::SensorTorques(const string& name) : TaskContext(name, PreOperational), Vmeasured(N, 0.0), Tmeasured(N, 0.0), Tjoint(N, 0.0)
+{
+	addProperty( "Ksensor", Ksensor).doc("Ksensor [Vm]");
+	addProperty( "Voffset", Voffset).doc("Voffset [V]");
+	addProperty( "Xoffset", Xoffset).doc("Xoffset [m]");
+	addProperty( "Stiffness", Stiffness).doc("Stiffness [N/m]");
+	addProperty( "PivotDistance", PivotDistance).doc("Distance from the force act point to the pivot point [m]");
+	
+	addEventPort("voltage_in", voltage_inport).doc("Optical sensors voltage [V]");
+	addPort("joint_torques_out", joint_torques_outport).doc("Joint torques [Nm]");
+	addPort("measured_torques_out", measured_torques_outport).doc("Differential (gear) torques [Nm]");
+	
+}
+SensorTorques::~SensorTorques(){}
+
+bool SensorTorques::configureHook()
+{	
+	return true;
+}
+
+bool SensorTorques::startHook()
+{
+	Logger::In in("SensorTorques::startHook()");
+	
+	if (Ksensor.size()!=N || Voffset.size()!=N || Xoffset.size()!=N || Stiffness.size()!=N || PivotDistance.size()!=N) {
+		log(Error)<<"Parameters missing! Check the sizes of Ksensor, Voffset, Stiffness and PivotDistance arrays."<< endlog();
+		return false;
+	}	
+	if (!voltage_inport.connected()) {
+		log(Error)<<"Inputport not connected!"<<endlog();
+		return false;
+	}
+	if (!joint_torques_outport.connected()) {
+		log(Warning)<<"Outputport1 not connected!"<<endlog();
+	}
+	if (!measured_torques_outport.connected()) {
+		log(Warning)<<"Outputport2 not connected!"<<endlog();
+	}
+	return true;
+}
+
+void SensorTorques::updateHook()
+{
+	voltage_inport.read(Vmeasured);
+	
+	for (unsigned int i=0; i<N; i++) {
+		Tmeasured[i] = (Ksensor[i]/(Vmeasured[i] + Voffset[i])-Xoffset[i])*Stiffness[i]*PivotDistance[i]; // Differential (gear) torques
+	}
+	
+	// Joint torques
+	Tjoint[0] =  Tmeasured[0] - Tmeasured[1];
+	Tjoint[1] = -(Tmeasured[0] + Tmeasured[1]);
+	Tjoint[2] =  Tmeasured[4];
+	Tjoint[3] =  Tmeasured[2] + Tmeasured[3];
+	Tjoint[4] =  Tmeasured[2] - Tmeasured[3];
+	Tjoint[5] =  Tmeasured[6] + Tmeasured[7];
+	Tjoint[6] =  Tmeasured[6] - Tmeasured[7];
+	Tjoint[7] =  Tmeasured[5];
+		
+	measured_torques_outport.write(Tmeasured);
+    joint_torques_outport.write(Tjoint);
+}
+
+ORO_CREATE_COMPONENT(SensorTorques)
