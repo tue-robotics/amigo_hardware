81d487ae71baa2392857a02aae7779d1a02ed5b2
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2012-02-10 14:56:40 +0000

    Restored amigo_base_controller to version in which reset IS working
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@7123 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ amigo_base_controller/src/BaseReset.hpp
@@ -0,0 +1,63 @@
+/** BaseReset.hpp
+ *
+ * @class BaseReset
+ *
+ * \author Tim Clephas
+ * \date May, 2011
+ * \version 1.0
+ *
+ */
+
+#ifndef BASERESET_HPP
+#define BASERESET_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <std_msgs/Bool.h>
+
+using namespace RTT;
+
+namespace AMIGO
+{
+  /**
+   * @brief
+   *
+   * @param No parameters
+   */
+
+  class BaseReset
+  : public RTT::TaskContext
+    {
+    private:
+
+    typedef vector<double> doubles;
+
+    /* Declaring and output port*/
+    InputPort<bool> safeport;
+    InputPort<std_msgs::Bool> rosresetport;
+    InputPort<std_msgs::Bool> rosemergencyport;
+    OutputPort<bool> resetport;
+
+    InputPort<doubles> posport;
+    OutputPort<doubles> integratorresetport;
+
+    /* Declaring property variables */
+    doubles max_velocities;
+    doubles max_errors;
+    double max_voltage;
+
+    /* Declaring global variables */
+    bool safe;
+
+    public:
+
+    BaseReset(const string& name);
+    ~BaseReset();
+
+    bool configureHook();
+    bool startHook();
+    void updateHook();
+
+    };
+}
+#endif
