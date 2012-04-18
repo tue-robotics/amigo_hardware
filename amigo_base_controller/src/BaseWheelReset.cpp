866cd99beac9de5f32abd808c8e8c6e7f53a7144
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2012-04-18 14:12:30 +0000

    Added BaseWheelReset to base controller
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@8111 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ amigo_base_controller/src/BaseWheelReset.cpp
@@ -0,0 +1,78 @@
+/** BaseWheelReset.cpp
+ *
+ * @class BaseWheelReset
+ *
+ * \author Janno Lunenburg
+ * \date April, 2012
+ * \version 1.0
+ *
+ */
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <rtt/Component.hpp>
+
+#include "BaseWheelReset.hpp"
+
+using namespace std;
+using namespace RTT;
+using namespace AMIGO;
+
+BaseWheelReset::BaseWheelReset(const string& name) :
+                                                    TaskContext(name, PreOperational)
+{
+
+    // Adding ports
+    addEventPort( "safe", safePort );
+    addEventPort( "emergency", rosEmergencyPort );
+    addEventPort( "inputRef", inputRefPort) ;
+    addPort( "measuredPos", measuredPosPort );
+
+    addPort( "outputRef", outputRefPort );
+
+}
+
+BaseWheelReset::~BaseWheelReset(){}
+
+bool BaseWheelReset::configureHook()
+{
+
+    return true;
+}
+
+bool BaseWheelReset::startHook()
+{
+
+
+    return true;
+}
+
+void BaseWheelReset::updateHook()
+{
+    std_msgs::Bool button;
+    rosEmergencyPort.read( button );
+    safePort.read( safe );
+
+    doubles refPos;
+
+    if ( !safe || button.data )
+    {
+
+        // In case of emergency button or unsafe situation:
+        // Use the measured position as input position for the feedback loop to ensure zero tracking error
+        measuredPosPort.read( refPos );
+
+    }
+
+    else
+    {
+
+        // During normal operation:
+        // Use the input reference position for the feedback loop
+        inputRefPort.read( refPos );
+
+    }
+    outputRefPort.write( refPos );
+}
+
+ORO_CREATE_COMPONENT(AMIGO::BaseWheelReset)
---
+++ amigo_base_controller/src/BaseWheelReset.hpp
@@ -0,0 +1,60 @@
+/** BaseWheelReset.hpp
+ *
+ * @class BaseWheelReset
+ *
+ * \author Janno Lunenburg
+ * \date April, 2012
+ * \version 1.0
+ *
+ */
+
+#ifndef BASEWHEELRESET_HPP
+#define BASEWHEELRESET_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <std_msgs/Bool.h>
+
+using namespace std;
+using namespace RTT;
+
+namespace AMIGO
+{
+  /**
+   * @brief Component that outputs the input references in case of normal operation and
+   * outputs the measured position in case of unsafe operation or pressed emergency button.
+   * Note that switching between both case does need an additional interpolator.
+   *
+   * @param No parameters
+   */
+
+  class BaseReset
+  : public RTT::TaskContext
+    {
+    private:
+
+    typedef vector<double> doubles;
+
+    /* Declaring and output port*/
+    InputPort<bool> safePort;
+    InputPort<std_msgs::Bool> rosEmergencyPort;
+    InputPort<doubles> inputRefPort;
+    InputPort<doubles> measuredPosPort;
+
+    OutputPort<doubles> outputRefPort;
+
+    /* Declaring global variables */
+    bool safe;
+
+    public:
+
+    BaseWheelReset(const string& name);
+    ~BaseWheelReset();
+
+    bool configureHook();
+    bool startHook();
+    void updateHook();
+
+    };
+}
+#endif
