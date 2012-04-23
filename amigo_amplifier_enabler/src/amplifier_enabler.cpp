a00f1f5819f9dad85ab7e4f8485a8306d3b9bdb6
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2012-04-23 18:24:24 +0000

    Added delay in amplifier enabler to prevent the base from going into safety when pushing. Since this basically a hack, this should be reverted as soon as it is fixed electrically
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@8242 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- amigo_amplifier_enabler/src/amplifier_enabler.cpp
+++ amigo_amplifier_enabler/src/amplifier_enabler.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-ros::Time time_init, time_current;
+ros::Time time_init, time_current, time_ebutton_enable;
 ros::Publisher amplifier_pub;
 ros::Subscriber diag_sub;
 ros::Subscriber emergency_sub;
@@ -20,8 +20,13 @@ statuses.push_back(status);
 diagnostic_msgs::DiagnosticArray diag_msg;
 */
 int battery_status = 0;
-bool emergency_switch_status = true;
+bool emergency_switch_status = true; /// Status of the incoming emergency switch
+bool ebutton_safe = true; /// Status is set to true if the emergency_switch_status is false for a certain duration
+bool previous_emergency_switch_status = false;
 std_msgs::Bool disable_amplifiers;
+ros::Duration emergency_switch_delay(0.5); /// Time between releasing the emergency switch and enabling amplifiers
+ros::Duration enabled_time; /// Time the ebutton has been enabled in ROS time
+//double enabled_time_sec; /// Time the ebutton has been enabled
 
 void diagnosticCallback(const diagnostic_msgs::DiagnosticArray::ConstPtr& msg)
 {
@@ -45,6 +50,26 @@ void diagnosticCallback(const diagnostic_msgs::DiagnosticArray::ConstPtr& msg)
 void emergencySwitchCallback(const std_msgs::Bool::ConstPtr& msg)
 {
 	emergency_switch_status=msg->data;
+	/// Set new emergency switch time when ebutton is pressed and upon release
+	if ( (emergency_switch_status == false && previous_emergency_switch_status == true) || emergency_switch_status == true){
+		
+		time_ebutton_enable = ros::Time::now();
+		
+	}
+	/*enabled_time = ros::Time::now() - time_ebutton_enable;
+	enabled_time_sec = enabled_time.toSec();
+	if (enabled_time_sec > emergency_switch_delay){
+		
+		ebutton_safe = true;
+		
+	}*/
+	if ( (ros::Time::now() - time_ebutton_enable) > emergency_switch_delay ){
+		ebutton_safe = true;
+	}
+	else {
+		ebutton_safe = false;
+	}
+	
 }
 
 int main(int argc, char **argv)
@@ -66,7 +91,8 @@ int main(int argc, char **argv)
 		ros::spinOnce();
 		
 		// Disable amplifiers when emergency switch is pushed or battery status equals 2 (= Voltage is too low)
-		if (battery_status != 2 && emergency_switch_status == false){
+		//if (battery_status != 2 && emergency_switch_status == false){
+		if (battery_status != 2 && ebutton_safe == true){
 			disable_amplifiers.data = false;
 			//ROS_WARN("Enable amplifiers");
 		}
@@ -75,6 +101,8 @@ int main(int argc, char **argv)
 			//ROS_WARN("Disable amplifiers");
 		}
 		amplifier_pub.publish(disable_amplifiers);
+		
+		previous_emergency_switch_status = emergency_switch_status;
 		
 		loop_rate.sleep();
 
