e36ee4a59406c09cff4b928a6c90bebd19e9d2d9
Max Baeten <m.j.j.baeten@student.tue.nl>
2013-04-03 19:54:00 +0000

    made backup of old driver and pushed my driver to trunk
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@14246 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- amigo_etherCAT/include/soem_armEthercat.hpp
+++ amigo_etherCAT/include/soem_armEthercat.hpp
@@ -25,7 +25,6 @@
  *                                                                         *
  ***************************************************************************/
 
-
 #ifndef SOEM_ARMETHERCAT_H
 #define SOEM_ARMETHERCAT_H
 
@@ -39,10 +38,7 @@
 #include "COE_config.h"
 #include <soem_beckhoff_drivers/AnalogMsg.h>
 #include <soem_beckhoff_drivers/EncoderMsg.h>
-
-#define RES_ENC1 255
-#define RES_ENC2 255
-#define RES_ENC3 255
+#include <std_msgs/Bool.h>
 
 using namespace std;
 typedef vector<double> doubles;
@@ -63,10 +59,7 @@ typedef struct PACKED {
 		float spare_analog_in_2; 		// Analog ADC Value for AD spare input 2
 		float supply_int_5V; 			// Analog ADC value of 5V supply voltage for ADC
 		float supply_int_12V; 			// Analog ADC value of 12V supply voltage for H bridges
-		float supply_ext_24V; 			// Analog ADC value of 12V main supply voltage
-		float current_motor_1; 			// Analog ADC value of motor current 1 sensor
-		float current_motor_2; 			// Analog ADC value of motor current 2 sensor
-		float current_motor_3; 			// Analog ADC value of motor current 3 sensor
+		float supply_ext_24V; 			// Analog ADC value of 24V main supply voltage
 		float supply_int_1V2; 			// Analog ADC value of 1.2V supply voltage for FPGA
 		float supply_int_1V65;	 		// Analog ADC value of 1.65 supply ADC vref voltage
 		uint32 message_index; 			// Message index counter
@@ -78,7 +71,6 @@ typedef struct PACKED {
 			uint16 pwm_duty_motor_2; 	// PWM duty cycle for motor 2
 			uint16 pwm_duty_motor_3; 	// PWM duty cycle for motor 3
 			uint8  spare_digital_out; 	// Spare I/O connector digital inputs
-			uint16 ramp_rate; 		  	// Motor acceleration ramp rate
 			uint8  heart_beat;			// Heart beat 
 	} out_armEthercatMemoryt;
 
@@ -93,64 +85,69 @@ typedef struct PACKED {
 
 		void update();
 		bool configure();
-		bool startHook();
         void write_pwm(float val1,float val2,float val3);
         void read_encoders();
         void read_supply();
         void read_forces();
         void read_positions();
         void read_spareanalog();
-        void read_currents();
-
         void stop();
 
-
-
 	private:
-		bool setOutputToZero;
-		uint16 encoderAngle0;
+        int printEnabled;
+        int printDisabled;
+        uint16 cntr;
+        uint16 cntr2;
+        uint16 cntr3;
+        bool enable;
+        bool setOutputToZero;
+        uint8 heart_beat_source;
+        uint16 initRelEnc1;
+        uint16 initRelEnc2;
+        uint16 initRelEnc3;
+        uint16 encoderAngle0;
 		uint16 encoderAngle1;
 		uint16 encoderAngle2;
-		std::vector<float> forceSensors;
-		std::vector<float> positionSensors;
-		std::vector<float> spareAnalogIns;     					// ToDo: Why call these spare???
-		std::vector<float> motorCurrents;
-		std::vector<float> supplyVoltages;
+	  	std::vector<float> forceSensors;
+	  	std::vector<float> positionSensors;
+	  	std::vector<float> spareAnalogIns;     					// ToDo: Why call these spare???
+	  	std::vector<float> motorCurrents;
+	    std::vector<float> supplyVoltages;
 		std::vector<float> pwmDutyMotors;		
 		
 		EncoderMsg encoderAngle0_msg;
 		EncoderMsg encoderAngle1_msg;
 		EncoderMsg encoderAngle2_msg;
-		AnalogMsg forceSensors_msg;
-		AnalogMsg positionSensors_msg;
-		AnalogMsg spareAnalogIns_msg;
-		AnalogMsg motorCurrents_msg;
-		AnalogMsg supplyVoltages_msg;
+	  	AnalogMsg forceSensors_msg;
+	  	AnalogMsg positionSensors_msg;
+	  	AnalogMsg spareAnalogIns_msg;
+	  	AnalogMsg motorCurrents_msg;
+	 	AnalogMsg supplyVoltages_msg;
 		AnalogMsg pwmDutyMotors_msg;
+        std_msgs::Bool enable_msg;
 		
-		
-		in_armEthercatMemoryt* m_in_armEthercat;
-		out_armEthercatMemoryt* m_out_armEthercat;
+        in_armEthercatMemoryt* m_in_armEthercat;
+        out_armEthercatMemoryt* m_out_armEthercat;
 
 		OutputPort<EncoderMsg> port_out_encoderAngle0;
 		OutputPort<EncoderMsg> port_out_encoderAngle1;		
 		OutputPort<EncoderMsg> port_out_encoderAngle2;
-		OutputPort<AnalogMsg> port_out_forceSensors;
-		OutputPort<AnalogMsg> port_out_positionSensors;
-		OutputPort<AnalogMsg> port_out_spareAnalogIns;
-		OutputPort<AnalogMsg> port_out_motorCurrents;
-		OutputPort<AnalogMsg> port_out_supplyVoltages;
-		
-		OutputPort< std::vector<bool> >  port_out_spareDigitalIns; // what does this do?
+	 	OutputPort<AnalogMsg> port_out_forceSensors;
+	 	OutputPort<AnalogMsg> port_out_positionSensors;
+	 	OutputPort<AnalogMsg> port_out_spareAnalogIns;
+	 	OutputPort<AnalogMsg> port_out_motorCurrents;
+	 	OutputPort<AnalogMsg> port_out_supplyVoltages;
+        // OutputPort<std::vector<Bool> >  port_out_spareDigitalIns;
 
 		// ToDo: Use messages similar to soem_beckhoff_drivers? 
-		// This basically holds for every port
-		
-		OutputPort	< std::vector<float> > 		port_out_pwmDutyMotors;   // deze poort gebruiken we nu niet
-		InputPort	<AnalogMsg>  				port_in_pwmDutyMotors;
-		InputPort	< std::vector<bool> >  		port_in_spareDigitalOuts;  
-		OutputPort	< std::vector<bool> >  		port_out_spareDigitalOuts;
-	
-	};
+		// This basically holds for every port		
+		OutputPort	< std::vector<float> > 		port_out_pwmDutyMotors;   
+        InputPort	< AnalogMsg>  				port_in_pwmDutyMotors;
+        InputPort   < bool>                     port_in_enable;
+        InputPort   < bool>                     port_in_reNullPort;
+	 	//InputPort	< std::vector<bool> >  		port_in_spareDigitalOuts;  
+        //OutputPort	< std::vector<bool> >  	port_out_spareDigitalOuts;
+
+    };
 	}
 #endif
