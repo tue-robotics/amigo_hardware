2fac538a4121e0fa8015b2b326eb1095d3dfee49
Janno Lunenburg <j.j.m.lunenburg@tue.nl>
2013-08-09 14:50:23 +0000

    Prepared PanTiltController for JointState messages, needs to become more generic though
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@17956 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ amigo_head_controller/src/PanTiltControllerJointState.hpp
@@ -0,0 +1,107 @@
+/***************************************************************************
+ tag: Janno Lunenburg  Fri August 13 16:00:00 CET 2013  PanTiltControllerJointState.hpp
+
+ PanTiltControllerJointState.hpp -  description
+ -------------------
+ begin                : Sat February 19 2011
+ copyright            : (C) 2011 Sava Marinkov
+ email                : s.marinkov@student.tue.nl
+
+ ***************************************************************************/
+
+#ifndef PANTILTCONTROLLERJOINTSTATE_HPP
+#define PANTILTCONTROLLERJOINTSTATE_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <rtt/Component.hpp>
+#include <soem_beckhoff_drivers/CommMsgBig.h>
+//#include <amigo_msgs/head_ref.h>
+//#include <std_msgs/Float64.h>
+#include <sensor_msgs/JointState.h>
+#include "AX12_control_table.h"
+#include <queue>
+
+#define RAD_TO_STEP             195.37860814
+#define TRIAL_MAX 				20
+
+using namespace std;
+using namespace RTT;
+using namespace soem_beckhoff_drivers;
+
+namespace AMIGO {
+
+typedef std::vector<double> doubles;
+
+class PanTiltControllerJointState : public RTT::TaskContext {
+	private:
+
+		OutputPort<CommMsgBig> instructionPort;
+		InputPort<CommMsgBig> statusPort;
+		InputPort<sensor_msgs::JointState> goalPosPort;
+		//OutputPort<std_msgs::Float64> currentPosPanPort;
+		//OutputPort<std_msgs::Float64> currentPosTiltPort;
+		OutputPort<sensor_msgs::JointState> currentPosPort;
+		
+		InputPort<bool> serialRunningPort;
+		InputPort<bool> serialReadyRxPort;
+		
+		CommMsgBig instruction;
+		CommMsgBig status;
+		//amigo_msgs::head_ref goalPos;
+		sensor_msgs::JointState goalPos;
+		int trial, newPosition;
+		//std_msgs::Float64 currentPan, currentTilt;
+		sensor_msgs::JointState currentPos;
+		
+		int state, pstate;
+		int commStatus;
+		unsigned char gbInstructionPacket[MAXNUM_TXPARAM+10];
+		unsigned char gbStatusPacket[MAXNUM_TXPARAM+10];
+		int gbStatusSize;
+		int pan_id, tilt_id, pan_goal, tilt_goal, pan_max, pan_min, tilt_max, tilt_min, pan_speed, tilt_speed, pan_offset, tilt_offset;
+	public:
+		PanTiltControllerJointState(const std::string& name);
+		~PanTiltControllerJointState(){};
+
+		bool configureHook();
+
+		bool startHook();
+	  
+		void updateHook();
+
+		void stopHook(){};
+	
+	private:
+		bool readReference();
+	
+		void dxl_set_txpacket_instruction(int instruction);
+		void dxl_tx_rx_packet(void);
+		void dxl_set_txpacket_parameter(int index, int value);
+		void dxl_set_txpacket_length(int length);
+		
+		void dxl_rx_packet(void);
+		int dxl_get_rxpacket_error(int errbit);
+		int dxl_rxpacket_isError(void);
+		int dxl_get_rxpacket_length(void);
+		int dxl_get_rxpacket_id(void);
+		int dxl_get_rxpacket_parameter(int index);
+		bool dxl_check_rxpacket_checksum(void);
+		
+		int dxl_makeword(int lowbyte, int highbyte);
+		int dxl_get_lowbyte(int word);
+		int dxl_get_highbyte(int word);
+
+		void dxl_ping(int id);
+		void dxl_read_byte(int id, int address);
+		void dxl_write_byte(int id, int address, int value);
+		void dxl_read_word(int id, int address);
+		void dxl_write_word(int id, int address, int value);
+		
+		void dxl_tx_packet(void);
+		void dxl_set_txpacket_id( int id );
+		void printErrorCode(void);
+
+};
+}
+#endif
