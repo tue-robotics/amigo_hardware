9982a04b04dae35cb44401923e503a50df157d43
Max Baeten <m.j.j.baeten@student.tue.nl>
2013-08-13 17:27:05 +0000

    added generic homing component
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@18018 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ amigo_spindle_controller/src/Homing.cpp
@@ -0,0 +1,201 @@
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <rtt/Component.hpp>
+#include <rtt/OperationCaller.hpp>
+
+#include "Homing.hpp"
+
+#include <ros/ros.h>
+
+using namespace std;
+using namespace RTT;
+using namespace AMIGO;
+
+
+Homing::Homing(const string& name) : TaskContext(name, PreOperational)
+  {
+  addEventPort( "endswitch", endSwitch_inport );
+  addPort( "abs_pos_in", absPos_inport );
+  addPort( "force_in", force_inport );
+  addPort( "servo_error_in", servoError_inport );
+  addPort( "ref_out", ref_outport );
+
+  // Creating variables
+  addProperty( "homing_body", homing_body );     // name of the body that is stopped/started during homing procedure
+  addProperty( "homing_type", homing_type );     // 0 = abs sen homing 1 = servo error homing  2 = Force sen homing  3 = endSwitch homing
+  addProperty( "homing_order", homing_order );   // TO DO : add different homing order
+  addProperty( "homing_refPos", homing_refPos ); // Pos Reference for the homing joint
+  addProperty( "homing_refVel", homing_refVel ); // Vel Reference for the homing joint
+  addProperty( "homing_stroke", homing_stroke ); // Stroke from zero point to homing point (encoders are resetted using this value)
+  addProperty( "homing_midpos", homing_midpos ); // position that the body should have during homing. To avoid collisions with other bodies/ itself
+  addProperty( "homing_endpos", homing_endpos ); // position that the body should go to after homing is finished. (could be same as midpos)
+  addProperty( "homing_absPos", homing_absPos ); // homing criterion for abs sen homing
+  addProperty( "homing_force", homing_force );   // homing criterion for Force sen homing
+  addProperty( "homing_error", homing_error );   // homing criterion for servo error homing
+
+}
+Homing::~Homing(){}
+
+bool Homing::configureHook()
+{	homed = false;
+    N = homing_refPos.size();
+    ref.resize(N); //N joint(s)
+    for( unsigned int j = 0; j < N; j++ )
+    {
+        ref[j].resize(3); //pos, vel, acc
+    }
+
+    // Lookup the Supervisor component.
+	TaskContext* Supervisor = this->getPeer("Supervisor");
+	if ( !Supervisor ) {
+        log(Error) << "Could not find Supervisor component! Did you add it as Peer in the ops file?"<<endlog(); // TODO check with Tim if removed checks are really neccesary and if these can be made generic
+		return false;
+	}		
+	// Lookup operations of peers
+	StartBodyPart = Supervisor->getOperation("StartBodyPart");
+	if ( !StartBodyPart.ready() ) {
+		log(Error) << "Could not find Supervisor.StartBodyPart Operation!"<<endlog();
+		return false;
+	}
+	StopBodyPart = Supervisor->getOperation("StopBodyPart");
+	if ( !StopBodyPart.ready() ) {
+		log(Error) << "Could not find Supervisor.StopBodyPart Operation!"<<endlog();
+		return false;
+	}	
+    return true;
+}
+
+bool Homing::startHook()
+{ 
+    JntNr = 1;
+    lastHomedJoint = 0;
+    GoToMidPos = false;
+    newRefSent = false;
+    HomingFinished = false;
+
+    // ToDo make SpindleReadSetpoint component generic
+
+    if ( !homed ) {
+        TaskContext* SpindleReadSetpoint = this->getPeer("SpindleReadSetpoint");
+        if ( ! SpindleReadSetpoint->isRunning() ) {
+            log(Error) << "Spindle component is not running yet, please start this component first" << endlog();
+        }
+        else {
+            SpindleReadSetpoint->stop(); //Disabling reading of references. Will be enabled automagically at the end by the supervisor.
+        }
+    }
+    else {
+        this->stop(); //Nothing to do
+    }
+
+  log(Warning)<<"SpindleHoming::started at " << os::TimeService::Instance()->getNSecs()*1e-9 <<endlog();
+
+
+  return true;
+}
+
+void Homing::updateHook()
+{
+	if (HomingFinished == false)
+    {
+        ref[JntNr-1][0] = 10.0; // You always find the endstop within 10 radians
+        ref[JntNr-1][1] = homing_refVel[JntNr-1];
+        ref[JntNr-1][2] = 0.0;  // May be To Do, add homing_refAcc?
+        ref_outport.write(ref);
+  
+        switch (homing_type[JntNr-1]) {
+            case 0 : 
+            {
+                if (abs(absPos[JntNr-1]-homing_absPos[JntNr-1]) <= 1) 
+                {
+                    log(Warning)<< "Homed a joint using absPos homing"  <<endlog();
+                    HomingFinished = true;
+                }
+            }
+            case 1 : 
+            {
+                servoError_inport.read(servoErrors);
+                if (fabs(servoErrors[JntNr-1]) >= homing_error[JntNr-1])
+                {
+                    log(Warning)<< "Homed a joint using endstop homing"  <<endlog();
+                    HomingFinished = true;
+                }
+            }
+            case 2 : 
+            {
+                force_inport.read(forces);
+                if (fabs(forces[JntNr-1]) >= homing_force[JntNr-1]) 
+                {
+                    log(Warning)<< "Homed a joint using forceSensor homing"  <<endlog();
+                    HomingFinished = true;
+                }
+            }
+            case 3 : 
+            {
+                endSwitch_inport.read(endSwitch);
+                if (!endSwitch.data) 
+                {
+					log(Warning)<< "Homed a joint using endswitch homing"  <<endlog();
+					HomingFinished = true; 
+                }
+            }
+        }
+    }
+
+    if (HomingFinished == true)
+    {
+        HomingFinished = false;
+        newRefSent = false;
+        lastHomedJoint = JntNr;
+        ROS_INFO_STREAM( "Joint X is homed" );
+        log(Warning)<< homing_body << " joint :" << JntNr << " is homed."  <<endlog();
+
+        // Actually call the services
+        StopBodyPart(homing_body);
+        ResetEncoder((JntNr-1),homing_stroke[JntNr-1]);
+        StartBodyPart(homing_body);
+
+        // send body joint to midpos, joints that are not homed yet are kept at zero
+        GoToMidPos = true;
+        ref[JntNr-1][0] = homing_midpos[JntNr-1];
+        ref[JntNr-1][1] = 0.0;
+        ref[JntNr-1][2] = 0.0;
+        ref_outport.write(ref);
+
+    }
+
+    if (GoToMidPos)  {              // this loop is used to send the joint to a position where it does not hinder other joints that needs to be homded of the same body part
+		log(Warning)<< "GoingToMidPos:" << homing_body  <<endlog();
+        relPos_inport.read(relPos);
+        if ( fabs(relPos[JntNr-1]-homing_midpos[JntNr-1]) <= 0.1) {
+            JntNr++;
+            GoToMidPos = false;
+        }
+    }
+
+    if ( JntNr == (N + 1) && (!GoToMidPos) ) // if Last Jnt is homed and mid pos is reached for the last joint go to end pos
+    {
+        ref[JntNr-1][0] = homing_endpos[JntNr-1];
+        ref[JntNr-1][1] = 0.0;
+        ref[JntNr-1][2] = 0.0;
+        ref_outport.write(ref);
+	
+        if ( fabs(relPos[JntNr-1]-homing_midpos[JntNr-1]) <= 0.1) {
+            JntNr++;
+            GoToMidPos = false;
+        }
+
+        homed = true;
+        log(Warning)<< "Finished homing of body:" << homing_body  <<endlog();
+
+        // Stop this component.
+        this->stop(); // stop Homing component when Homing is finished
+    }
+}
+
+void Homing::stopHook()
+{ 
+    log(Warning)<< "Killed homing component of body:" << homing_body  <<endlog();
+}
+
+ORO_CREATE_COMPONENT(Homing)
---
+++ amigo_spindle_controller/src/Homing.hpp
@@ -0,0 +1,82 @@
+#ifndef HOMING_HPP
+#define HOMING_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <std_msgs/Bool.h>
+
+using namespace std;
+using namespace RTT;
+
+namespace AMIGO // Just because it looks nice
+{
+  // Define a new type for easy coding:
+  typedef vector<double> doubles;
+  typedef vector<int> ints;
+
+  class Homing
+  : public RTT::TaskContext
+    {
+    private:
+    // Declaring Inports
+    InputPort<std_msgs::Bool> endSwitch_inport;
+    InputPort<ints> absPos_inport;
+    InputPort<doubles> force_inport;
+    InputPort<doubles> servoError_inport;
+    InputPort<doubles> relPos_inport;
+
+    // Declaring Outports
+    OutputPort< vector<doubles> > ref_outport;
+
+
+    // Declaring system variables
+    bool homed;
+    bool HomingFinished;
+    bool GoToMidPos;
+    bool newRefSent;
+    uint N;
+    uint JntNr;
+    uint lastHomedJoint;
+
+    // homing variables
+    string homing_body;
+    ints homing_type;
+    ints homing_order;
+    doubles homing_refPos;
+    doubles homing_refVel;
+    doubles homing_midpos;
+    doubles homing_endpos;
+    doubles homing_stroke;
+
+    //ref
+    vector<doubles> ref;
+
+    // Current value variables
+    ints absPos;
+    std_msgs::Bool endSwitch;
+    doubles servoErrors;
+    doubles forces;
+    doubles relPos;
+
+    // Homing criterion variables
+    ints homing_absPos;
+    doubles homing_force;
+    doubles homing_error;
+
+    protected:
+    OperationCaller<bool(string)> StartBodyPart;
+    OperationCaller<bool(string)> StopBodyPart;
+    OperationCaller<void(int,double)> ResetEncoder;
+
+    public:
+
+    Homing(const string& name);
+    ~Homing();
+
+    bool configureHook();
+    bool startHook();
+    void updateHook();
+    void stopHook();
+    };
+}
+#endif
