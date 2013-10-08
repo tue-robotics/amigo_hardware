5c139afe4bf21a435d4a06679c13b3ef291ce9ef
Max Baeten <m.j.j.baeten@student.tue.nl>
2013-10-08 20:58:39 +0000

    work on gripper, WIP
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@18925 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- amigo_pera_controller/src/PERA_GripperControl.cpp
+++ amigo_pera_controller/src/PERA_GripperControl.cpp
@@ -101,7 +101,7 @@ using namespace PERA;
 
 			if(gripperCommand.direction == amigo_msgs::AmigoGripperCommand::OPEN){
 				if (gripperPos[0] >= maxPos){
-					//log(Info)<<"Gripper is OPEN"<<endlog();
+					log(Warning)<<"Gripper is OPEN"<<endlog();
 					gripperMeasurement.end_position_reached = true;
 					completed = true;
 				} 
@@ -111,7 +111,7 @@ using namespace PERA;
 			} 
 			else{
 				//log(Warning)<<"gripper torques = "<<torques[GRIPPER_JOINT_INDEX_MOTORSPACE]<<endlog();
-				if ( (torques[GRIPPER_JOINT_INDEX_MOTORSPACE] >= threshold_closed && torques[GRIPPER_JOINT_INDEX_MOTORSPACE] < MAX_TORQUE) || ( gripperHomed && (gripperPos[0] < 0.0)) ){
+				if ( (torques[GRIPPER_JOINT_INDEX_MOTORSPACE] >= threshold_closed && torques[GRIPPER_JOINT_INDEX_MOTORSPACE] < MAX_TORQUE) || ( gripperPos[0] < 0.0) ){
 					log(Warning)<<"Gripper is CLOSED"<<endlog();
 					gripperMeasurement.end_position_reached = true;
 					completed = true;
