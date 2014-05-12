586afe5fb08b714793b14a386f3ab622765e60eb
Max Baeten <m.j.j.baeten@student.tue.nl>
2014-05-12 09:18:06 +0000

    finished new sensortorques component, with online c3 estimation
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_actuation@24173 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- amigo_pera_controller/src/SensorTorquesParamEstimator.cpp
+++ amigo_pera_controller/src/SensorTorquesParamEstimator.cpp
@@ -12,8 +12,7 @@ SensorTorquesParamEstimator::SensorTorquesParamEstimator(const string& name) : T
 {
 	addProperty( "c1", c1).doc("Calibration Coefficient c1");
 	addProperty( "c2", c2).doc("Calibration Coefficient c2");
-	addProperty( "c3", c3).doc("Calibration Coefficient c3");
-	
+		
 	addEventPort("voltage_in", voltage_inport).doc("Optical sensors voltage [V]");
 	addPort("measured_torques_out", measured_torques_outport).doc("Differential (gear) torques [Nm]");
 	
@@ -22,6 +21,7 @@ SensorTorquesParamEstimator::~SensorTorquesParamEstimator(){}
 
 bool SensorTorquesParamEstimator::configureHook()
 {	
+	c3.assign(N,0.0);
 	return true;
 }
 
@@ -49,36 +49,39 @@ bool SensorTorquesParamEstimator::startHook()
 
 void SensorTorquesParamEstimator::updateHook()
 {
-	// read size 9 input doubles
+	// Convert size 9 inpute voltage to 8 size for output (obsolete third degree of freedom is removed)
 	voltage_inport.read(Vmeasured_in);
-	// remove obsolete joint from doubles
+	
 	Vmeasured[0] = Vmeasured_in[0];
 	Vmeasured[1] = Vmeasured_in[1];
 	for (unsigned int i=2; i<N; i++) {
 		Vmeasured[i] = Vmeasured_in[i+1];
 	}
-	
+
+	// Calculate Tmeasured
 	for (unsigned int i=0; i<N; i++) {
-        Tmeasured[i] = (c1[i]/(Vmeasured[i] + c2[i])+c3[i]); // Differential (gear) torques
+        Tmeasured[i] = (c1[i]/(Vmeasured[i] + c2[i])+c3[i]);
 	}
 	
+	// Estimation step, c3 is initialised at zero and in this step c3 is estimated
 	if (EstimationComplete == false) {
 		if (cntr >= 500  && EstimationStarted == false) {
 			EstimationStarted = true;
 			cntr = 0;
 		}
+		else if (EstimationStarted == true) {
+			doubles Tmean_(N, 0.0);
+			Tmean_ = Tmean;
+			for (unsigned int i=0; i<N; i++) {
+				Tmean[i] = (Tmean_[i]*cntr + Tmeasured[i]) / (cntr + 1);
+			}
+		}
 		else if (cntr >= 1000  && EstimationStarted == true) {
 			EstimationComplete = true;
-			c3 = 0.0;//
-		}
-		else if (EstimationStarted == true) {
-			doubles Tmean_(N, 0.0)
-			Tmean_ = Tmean
-			for (unsigned int i=0; i<N; i++) {
-				Tmean[i] = (Tmean_[i]*(double cntr) + Tmeasured[i])/((double cntr)+1)
-			}
+			c3 = Tmean;
+			log(Warning) << "SensorTorquesParamEstimator: Calculated c3 coefficients : [" << c3[0] << "," << c3[1] << "," << c3[2] << "," << c3[3] << "," << c3[4] << "," << c3[5] << "," << c3[6] << "," << c3[7] << "]" <<endlog();
 		}		
-		cntr++
+		cntr++;
 	}
 	else {
 		measured_torques_outport.write(Tmeasured);		
